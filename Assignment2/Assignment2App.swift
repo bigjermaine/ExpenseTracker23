afa8e494e5b9226cea00d28c7292589e0b2efe39
daniel jermaine <danieljermaine2@gmail.com>
2022-09-28 02:32:11 +0100

    Initial Commit

---
+++ Assignment2/Assignment2App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment2App.swift
+//  Assignment2
+//
+//  Created by Apple on 28/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Assignment2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
